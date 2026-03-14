25ef458f3882f19d0c0fd9c5789e77bb06f7fc0f
Lewis Steele <lewis.steele@me.com>
2026-03-14 14:09:04 +0000

    Remove comments

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,3 @@
--- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -15,9 +14,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
@@ -28,7 +24,6 @@ vim.opt.spell = true
 vim.opt.spelllang = "en_gb"
 vim.opt.tabstop = 2
 
--- Setup lazy.nvim
 require("lazy").setup({
   spec = {
     {
@@ -81,10 +76,6 @@ require("lazy").setup({
       config = true,
     },
   },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  -- automatically check for plugin updates
-  checker = { enabled = true, notify = false },
 })
 
 vim.lsp.config("gopls", {

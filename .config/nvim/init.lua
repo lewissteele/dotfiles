236e2cb66acbe4a746535355cfb87eda89a9acef
Lewis Steele <lewis.steele@me.com>
2026-03-14 11:54:56 +0000

    Install telescope

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -43,6 +43,20 @@ require("lazy").setup({
       "lewis6991/gitsigns.nvim",
       config = true,
     },
+    {
+      "nvim-telescope/telescope.nvim",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+      },
+      config = function()
+        local builtin = require("telescope.builtin")
+        vim.keymap.set("n", "<leader>ff", builtin.find_files)
+        vim.keymap.set("n", "<leader>fg", builtin.live_grep)
+        vim.keymap.set("n", "<leader>fb", builtin.buffers)
+        vim.keymap.set("n", "<leader>fh", builtin.help_tags)
+      end,
+    }
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
